0812cb2d60a3f697476a864cd17cfc9751bf5070
ShingsiHairolla <93879495+Shyngys001@users.noreply.github.com>
2023-07-15 21:24:51 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,15 +8,31 @@ let book1 = document.getElementById('book1');
 let price1 = document.getElementById('price1');
 let tour1 = document.getElementById('tour1');
 let contact1 = document.getElementById('contact1');
-let gen2 = document.getElementById('gen2');
-let book2 = document.getElementById('book2');
-let price2 = document.getElementById('price2');
-let tour2 = document.getElementById('tour2');
 let shyn_Page1Foot_p = document.getElementById('shyn_Page1Foot_p');
 let shyn_Page1Foot1_num = document.getElementById('shyn_Page1Foot1_num');
 let shyn_otzyvy1 = document.getElementById('shyn_otzyvy1');
 let shyn_otzyvy2 = document.getElementById('shyn_otzyvy2');
 let shyn_otzyvy3 = document.getElementById('shyn_otzyvy3');
+let vzroslDeti = document.getElementById('vzrosl_deti');
+let glamping4mest = document.getElementById('glamping_4mest');
+let shynDopusl = document.getElementById('shyn_dopusl');
+let vhod1 = document.getElementById('vhod1');
+let vhod2 = document.getElementById('vhod2');
+let vhod3 = document.getElementById('vhod3');
+let vhod4 = document.getElementById('vhod4');
+let vhod5 = document.getElementById('vhod5');
+let ot120 = document.getElementById('ot_120');
+let vStoimost = document.getElementById('v_stoimost');
+let zavtrak = document.getElementById('zavtrak');
+let bannye = document.getElementById('bannye');
+let vzroslyh = document.getElementById('vzroslyh');
+let zona = document.getElementById('zona');
+let grill = document.getElementById('grill');
+let chainye = document.getElementById('chainye');
+let wifi = document.getElementById('wifi');
+let pozvonit = document.getElementById('pozvonit');
+let glamping_standard = document.getElementById('glamping_standard');
+
 
 let arch_text_activities = document.getElementById("arch--text-activities");
 let arch_text_yachting = document.getElementById("arch--text-yachting");
@@ -35,7 +51,6 @@ let arch_text_pool = document.getElementById("arch--text-pool");
 let arch_text_bbq = document.getElementById("arch--text-bbq");
 let arch_text_sauna = document.getElementById("arch--text-sauna");
 let arch_text_price = document.getElementById("arch--text-price");
-let arch_footer_location_info = document.getElementById("arch--footer-location-info");
 
 
 function setLanguage(language) {
@@ -43,6 +58,26 @@ function setLanguage(language) {
     document.getElementById('languageBtn').innerText = language;
     
     if (language === 'Kz') {
+        vzroslDeti.textContent = '2 ересек / 2 бала';
+        glamping4mest.textContent = 'Глампинг стандарты 4 орындық';
+        shynDopusl.textContent = 'қосымша қызметтер';
+        vhod1.textContent = 'ересектерге  --- 6000₸';
+        vhod2.textContent = '6-дан 12 жасқа дейінгі балаларға  --- 3000₸';
+        vhod3.textContent = '6 жасқа дейінгі балаларға  --- тегін';
+        vhod4.textContent = 'глампинг қонақтарына арналған монша --- 15000₸';
+        vhod5.textContent = 'глампингсіз қонақтарға арналған монша --- 120000₸';
+        ot120.textContent = ' 120.000₸ бастап';
+        vStoimost.textContent = 'Ішіне кіреді';
+        zavtrak.textContent = 'таңғы ас';
+        bannye.textContent = 'ванна және сұлулық керек-жарақтары';
+        vzroslyh.textContent = '3 ересек бассейн және 1 балалар';
+        zona.textContent = 'йога аймағы, жағажай, автотұрақ, шезлонгтар';
+        grill.textContent = 'Террассадағы Гриль аймағы, гамак';
+        chainye.textContent = 'шай ыдыстары';
+        wifi.textContent = 'Wi-fi';
+        pozvonit.textContent = 'Байланысу';
+        glamping_standard.textContent = 'Глампинг стандарты'
+
         gen.textContent = 'Басты'
         book.textContent = 'Броньдау'
         price.textContent = 'Бағалар'
@@ -53,10 +88,6 @@ function setLanguage(language) {
         price1.textContent = 'Бағалар'
         tour1.textContent = 'Турлар'
         contact1.textContent = 'Байланысу'
-        gen2.textContent = 'Басты'
-        book2.textContent = 'Броньдау'
-        price2.textContent = 'Бағалар'
-        tour2.textContent = 'Турлар'
         shyn_otzyvy1.textContent = 'Бұл оңаша орындар табиғаттың қақ ортасында, қаланың қарбаласынан алыс орналасқан.'
         shyn_otzyvy2.textContent = 'Біз сізге күнделікті уайымдарды ұмытып, толық демалуға болатын жайлы баспана ұсынамыз.'
         shyn_otzyvy3.textContent = 'Медитация, стрессті жеңілдету, табиғатқа ену және құстардың әнін тыңдау үшін тамаша орын...'
@@ -79,9 +110,27 @@ function setLanguage(language) {
         arch_text_bbq.textContent = "ГРИЛЬ ЖӘНЕ БАРБЕКЮ АЙМАҒЫ";
         arch_text_sauna.textContent = "СКАНДИНАВИЯ МОНШАСЫ";
         arch_text_price.textContent = "БАҒАСЫ";
-        arch_footer_location_info.textContent = "Қазақстан Республикасы Маңғыстау облысы, Мұнайлы ауданы, Қызылқұм жері, 2/27 учаскесі";
         
     } else if (language === 'En') {
+        vzroslDeti.textContent = '2 adults | 2 children';
+        glamping4mest.textContent = 'Standard 4-person Glamping';
+        shynDopusl.textContent = 'Additional Services';
+        vhod1.textContent = 'Entrance for adults      6000₸';
+        vhod2.textContent = 'Entrance for children (6-12 years old)      3000₸';
+        vhod3.textContent = 'Entrance for children (under 6 years old)      Free';
+        vhod4.textContent = 'Sauna for Glamping guests      15000₸';
+        vhod5.textContent = 'Sauna for non-Glamping guests      120000₸';
+        ot120.textContent = 'from 120,000₸';
+        vStoimost.textContent = 'Includes';
+        zavtrak.textContent = 'Breakfast';
+        bannye.textContent = 'Bath and cosmetic accessories';
+        vzroslyh.textContent = '3 adult pools and 1 children\'s pool';
+        zona.textContent = 'Yoga zone, beach, parking, sun loungers';
+        grill.textContent = 'Grill area on the terrace, hammock';
+        chainye.textContent = 'Tea accessories';
+        wifi.textContent = 'Wi-Fi';
+        pozvonit.textContent = 'Contact';
+
         gen.textContent = 'Home'
         book.textContent = 'Book'
         price.textContent = 'Prices'
@@ -92,10 +141,6 @@ function setLanguage(language) {
         price1.textContent = 'Prices'
         tour1.textContent = 'Tours'
         contact1.textContent = 'Contact'
-        gen2.textContent = 'Home'
-        book2.textContent = 'Book'
-        price2.textContent = 'Prices'
-        tour2.textContent = 'Tours'
         shyn_otzyvy1.textContent = 'These secluded corners are located far from the city noise and hustle, in the heart of nature.'
         shyn_otzyvy2.textContent = 'We offer you a cozy sanctuary where you can forget about everyday worries and fully relax.'
         shyn_otzyvy3.textContent = 'An ideal place for meditation, stress relief, immersing yourself in nature, and listening to the singing of birds...'
@@ -118,9 +163,28 @@ function setLanguage(language) {
         arch_text_bbq.textContent = "GRILL AND BBQ AREA";
         arch_text_sauna.textContent = "SCANDINAVIAN SAUNA";
         arch_text_price.textContent = "PRICE";
-        arch_footer_location_info.textContent = "Republic of Kazakhstan Mangystau region, Munailinsky district, Kyzylkum locality, plot 2/27";
 
     } else if (language === 'Ru') {
+        vzroslDeti.textContent = '2 взрослых | 2 детей';
+        glamping4mest.textContent = 'Глэмпинг стандарт 4-х местный';
+        shynDopusl.textContent = 'Дополнительные услуги';
+        vhod1.textContent = 'Вход взрослым --- 6000₸';
+        vhod2.textContent = 'Вход детям с 6 до 12 лет  ---    3000₸';
+        vhod3.textContent = 'Вход детям до 6 лет   ---   Бесплатно';
+        vhod4.textContent = 'Баня для гостей глэмпинга  ---    15000₸';
+        vhod5.textContent = 'Баня для гостей без глэмпинга  ---    120000₸';
+        ot120.textContent = 'от 120.000₸';
+        vStoimost.textContent = 'Входит';
+        zavtrak.textContent = 'Завтрак';
+        bannye.textContent = 'Банные и косметические принадлежности';
+        vzroslyh.textContent = '3 взрослых бассейна и 1 детский';
+        zona.textContent = 'Зона для йоги, пляж, парковка, шезлонги';
+        grill.textContent = 'Гриль зона на террасе, гамак';
+        chainye.textContent = 'Чайные принадлежности';
+        wifi.textContent = 'Wi-fi';
+        pozvonit.textContent = 'Связаться';
+        glamping_standard.textContent = 'Глэмпинг Стандард'
+
         gen.textContent = 'Главное'
         book.textContent = 'Бронировать'
         price.textContent = 'Цены'
@@ -131,10 +195,6 @@ function setLanguage(language) {
         price1.textContent = 'Цены'
         tour1.textContent = 'Туры'
         contact1.textContent = 'Контакты'
-        gen2.textContent = 'Главное'
-        book2.textContent = 'Бронировать'
-        price2.textContent = 'Цены'
-        tour2.textContent = 'Туры'
         shyn_otzyvy1.textContent = 'Эти уединенные уголки находятся вдали от городского шума и суеты, в самом сердце природы.'
         shyn_otzyvy2.textContent = 'Предлагаем вам уютное убежище, где вы сможете забыть о повседневных заботах и полностью расслабиться.'
         shyn_otzyvy3.textContent = 'Идеальное место для медитации, снять стресс, расствориться в природе, слушать пение птиц...'
@@ -157,6 +217,5 @@ function setLanguage(language) {
         arch_text_bbq.textContent = "ГРИЛЬ И BBQ ЗОНА";
         arch_text_sauna.textContent = "СКАНДИНАВСКИЕ БАНИ";
         arch_text_price.textContent = "Цены";
-        arch_footer_location_info.textContent = "Республика Казахстан Мангистауская область, Мунайлинский район, местность Кызылкум, участок 2/27";
     }
 }
