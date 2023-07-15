9abeef43798bf5f2371952dd6f12bf45808ac1a3
ShingsiHairolla <93879495+Shyngys001@users.noreply.github.com>
2023-07-15 21:24:23 +0600

    Update arch.js

--- js/arch.js
+++ js/arch.js
@@ -6,6 +6,7 @@ let switch_arch_to_home = document.getElementById("arch--switch-home-page-button
 let switch_arch_block = document.getElementById("arch--switch-block");
 let activity_page = document.getElementById("arch--activity-section");
 let page_header = document.getElementById("home-page-header");
+let go_tomain_whenClick_Prices = document.getElementById("arch--switch-price-page-link");
 
 switch_home_to_arch.addEventListener("click", function() {
   switch_arch_block.classList.add("arch--switch-block-animate");
@@ -50,6 +51,21 @@ switch_home_to_arch2.addEventListener("click", function() {
 });
 
 switch_arch_to_home.addEventListener("click", function() {
+  switch_arch_block.classList.add("arch--switch-block-animate");
+
+  setTimeout(function() {
+    body.classList.remove("body-style");
+    activity_page.style.display = "none";
+    page_header.style.display = "flex";
+  }, 2000);
+
+  setTimeout(function() {
+    switch_arch_block.classList.remove("arch--switch-block-animate");
+  }, 4000);
+});
+
+
+go_tomain_whenClick_Prices.addEventListener("click", function() {
   switch_arch_block.classList.add("arch--switch-block-animate");
 
   setTimeout(function() {
