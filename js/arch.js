fefbc59593fd933fe8e28bcdd4f13872194a5639
Arshynbeek <135094895+Arshynbeek@users.noreply.github.com>
2023-07-15 16:58:50 +0600

    Update arch.js

--- js/arch.js
+++ js/arch.js
@@ -1,6 +1,7 @@
 let body = document.getElementById("body");
 let switch_home_to_arch = document.getElementById("tour");
-let switch_home_to_arch_burger = document.getElementById("tour1");
+let switch_home_to_arch1 = document.getElementById("tour1");
+let switch_home_to_arch2 = document.getElementById("tour2");
 let switch_arch_to_home = document.getElementById("arch--switch-home-page-button");
 let switch_arch_block = document.getElementById("arch--switch-block");
 let activity_page = document.getElementById("arch--activity-section");
@@ -20,7 +21,21 @@ switch_home_to_arch.addEventListener("click", function() {
   }, 4000);
 });
 
-switch_home_to_arch_burger.addEventListener("click", function() {
+switch_home_to_arch1.addEventListener("click", function() {
+  switch_arch_block.classList.add("arch--switch-block-animate");
+
+  setTimeout(function() {
+    body.classList.add("body-style");
+    activity_page.style.display = "initial";
+    page_header.style.display = "none";
+  }, 1650);
+
+  setTimeout(function() {
+    switch_arch_block.classList.remove("arch--switch-block-animate");
+  }, 4000);
+});
+
+switch_home_to_arch2.addEventListener("click", function() {
   switch_arch_block.classList.add("arch--switch-block-animate");
 
   setTimeout(function() {
